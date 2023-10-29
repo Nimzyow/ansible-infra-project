fe0259d416e81df5d90e2c94378f51e951dfacf0
Nima Soufiani <n_soufiani@hotmail.com>
2023-10-29 09:58:05 +0000

    setup django server

--- backend/django_project/urls.py
+++ backend/django_project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from hello.views import hello_world
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", hello_world, name="hello_world"),
 ]
