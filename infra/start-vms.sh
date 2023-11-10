c57e08c03f7ff514fb46be9a5b81c5b7c568966f
Nima Soufiani <n_soufiani@hotmail.com>
2023-11-10 21:40:22 +0000

    get django into vm

--- infra/start-vms.sh
+++ infra/start-vms.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# -e = this option makes the bash script exit immediately upon a non zero exit code
+# -u = this option treats unset variables as an error and causes the script to error
+# if you try to use a variable that has not been set.
 set -eu
 
 USER="${USER}"
