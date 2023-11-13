f2fccdbaf82457d234520affdd27ca15df567f04
Nima Soufiani <n_soufiani@hotmail.com>
2023-11-13 20:10:14 +0000

    add ansible and vagrant installation instructions

--- infra/start-vms.sh
+++ infra/start-vms.sh
@@ -10,12 +10,24 @@ RESET=$(tput sgr0)
 USER="${USER}"
 
 which ansible
+ansible_installed_status="$($?)"
+which vagrant
+vagrant_installed_status="$($?)"
 
-if [$? -ne 0]
+if [ansible_installed_status -ne 0]
   then
+    echo "Please install Ansible\n\n"
+    echo "Installation instructions\n"
+    echo "python3 -m pip install --user ansible"
     exit 1
-  else
-    python3 -m pip install --user ansible
+fi
+
+if [vagrant_installed_status -ne 0]
+  then
+    echo "Please install Vagrant\n\n"
+    echo "Installation instructions:\n"
+    echo "brew tap hashicorp/tap\nbrew install hashicorp/tap/hashicorp-vagrant"
+    exit 1
 fi
 
 echo "${GREEN}Starting main playbook${RESET}"
