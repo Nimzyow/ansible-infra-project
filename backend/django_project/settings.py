64cbf1af7a5d5d415d0b6df6af0100077e2f9b03
Nima Soufiani <n_soufiani@hotmail.com>
2024-04-21 23:25:33 +0100

    add postgres python driver and add database connection details

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "acme",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "10.2.2.206",
+        "PORT": "5432",
     }
 }
 
